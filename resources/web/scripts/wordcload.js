66462d2e0f3ae3119eca6a80f3281d5ec0fbc61d
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2015-02-10 14:31:11 +0330

    Add wordcloard.js and link it to the html

--- resources/web/scripts/wordcload.js
+++ resources/web/scripts/wordcload.js
@@ -1,3 +1,17 @@
 var w = window.innerWidth;
 var h = window.innerHeight;
 
+for(i=0; i<30; i++)
+{
+	var top = Math.floor(Math.random() * h);
+	var right = Math.floor(Math.random() * w);
+	
+	// creat the word in space
+	var div = document.createElement('div');
+	div.id = 'd'+i;
+	div.className = 'word';
+	div.style.right = right + "px";
+	div.style.top = top + "px";
+	div.innerHTML = "word" + i;
+	document.getElementsByTagName('body')[0].appendChild(div);
+}
