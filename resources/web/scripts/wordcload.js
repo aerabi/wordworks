0316c3cc44d16804ce3a426407cc109f26111c84
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2015-02-10 14:41:22 +0330

    Fix bug with DOM style top property

--- resources/web/scripts/wordcload.js
+++ resources/web/scripts/wordcload.js
@@ -1,17 +1,17 @@
-var w = window.innerWidth;
 var h = window.innerHeight;
+var w = window.innerWidth;
 
 for(i=0; i<30; i++)
 {
-	var top = Math.floor(Math.random() * h);
-	var right = Math.floor(Math.random() * w);
+	var divTop = Math.floor(Math.random() * h);
+	var divRight = Math.floor(Math.random() * w);
 	
 	// creat the word in space
 	var div = document.createElement('div');
 	div.id = 'd'+i;
 	div.className = 'word';
-	div.style.right = right + "px";
-	div.style.top = top + "px";
-	div.innerHTML = "word" + i;
+	div.style.top = divTop + "px";
+	div.style.right = divRight + "px";
+	div.innerHTML = "<a>word" + i + "</a>";
 	document.getElementsByTagName('body')[0].appendChild(div);
 }
