c219d319fa5af0fe49a196a443973c9f24ba8517
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2015-02-13 20:16:46 +0330

    Connect wordtree to the index

--- wordworks-php/scripts/index.js
+++ wordworks-php/scripts/index.js
@@ -2,6 +2,6 @@ function updateCload(word)
 {
 	if(word.length > 2)
 	{
-		document.getElementById('cload').src = "wordcload.php?word=" + word;
+		document.getElementById('cload').src = "wordtree.php?word=" + word;
 	}
 }
