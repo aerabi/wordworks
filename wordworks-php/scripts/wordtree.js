0b9db1ce8abae3c6a6376969aac09ae67e8a71b5
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2015-02-13 17:38:04 +0330

    Add wordtree

---
+++ wordworks-php/scripts/wordtree.js
@@ -0,0 +1,78 @@
+var h = window.innerHeight - 30;
+var w = window.innerWidth - 60;
+
+function fillTheTree(str, word, x, y, parent)
+{
+	var list = str.split("<br>");
+	
+	// finding radius
+	var r = Math.min(h - y, Math.min(w - x, y));
+	
+	for(i=0; i<list.length; i++)
+	{
+		var offset = 5;
+		var divTop = y + r * Math.cos(Math.PI * (i + offset) / (list.length + 2 * offset));
+		var divRight = x + r * Math.sin(Math.PI * (i + offset) / (list.length + 2 * offset));
+		
+		// creat the word in space
+		var div = document.createElement('div');
+		div.className = "word";
+		div.id = word + i;
+		div.style.top = divTop + "px";
+		div.style.right = divRight + "px";
+		div.innerHTML = '<a onclick="fillWithWord(this.innerHTML, ' + divRight + ', ' + divTop + ', \'' + (word + i) + '\')">' + list[i] + "</a>";
+		document.getElementsByTagName('body')[0].appendChild(div);
+	}
+	
+	for(i=0; i<list.length; i++)
+	{
+		// connect them to parent
+		jsPlumb.ready(function() {
+            jsPlumb.connect({
+                source:word+i,
+                target:parent,
+                endpoint:[ "Rectangle", { width:2, height:2 } ],
+				anchors:["Right", "Left"],
+				paintStyle:{lineWidth:2, strokeStyle:'rgb(243,18,18)'}
+            });
+        });
+	}
+}
+
+function fillWithURL(url, word, x, y, id)
+{
+	$.ajax({
+		url: url,
+		type: 'GET',
+		success: function(responce){
+			fillTheTree(responce, word, x, y, id);
+		},
+		error: function(xhr, textStatus, errorThrown){
+			alert('request failed\nxhr: ' + xhr.status + '\ntext status: ' + textStatus + '\nerror: ' + errorThrown);
+		}
+    });
+}
+
+function fillWithWord(word, x, y, id)
+{
+	if(x == -1) 
+		x = 0;
+	if(y == -1)
+		y = h/2;
+	
+	if(id == "null")
+	{
+		var div = document.createElement('div');
+		div.className = "word";
+		div.id = word;
+		div.style.top = y + "px";
+		div.style.right = x + "px";
+		div.innerHTML = word;
+		document.getElementsByTagName('body')[0].appendChild(div);
+		
+		id = word;
+	}
+	
+	//$(".word").remove();
+	fillWithURL("synonym.php?word=" + word, word, x, y, id)
+}
---
+++ wordworks-php/wordtree.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_GET['word']))
+	$word = $_GET['word'];
+else
+	$word = "واژه";
+?>
+
+<html lang="en">
+
+	<head>
+		<script src="scripts/jquery-2.1.0.min.js"></script>
+		<script src="scripts/jquery-ui.js"></script>
+		<script src="scripts/jquery.jsPlumb-1.4.1-all-min.js"></script>
+
+		<meta charset="utf-8">
+		<meta name="description" content="Word Tree">
+		<title>Word Tree</title>
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+		<LINK href="images/SquareAngellandros.ico" rel="SHORTCUT ICON">
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	</head>
+
+	<body>
+	
+		<script src="scripts/wordtree.js"></script>
+		<script lang="javascript">
+			fillWithWord("<?php echo $word ?>", -1, -1, "null");
+		</script>
+		
+	</body>
+
+</html>
