415fb05f692c974507966ce500f29eb5218d5d92
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2015-02-13 20:36:00 +0330

    Fix self-refrence

--- wordworks-php/scripts/wordtree.js
+++ wordworks-php/scripts/wordtree.js
@@ -1,6 +1,8 @@
 var h = window.innerHeight - 30;
 var w = window.innerWidth - 60;
 
+var expandeds = []
+
 function fillTheTree(str, word, x, y, parent)
 {
 	var list = str.substring(0, str.length-4).split("<br>");
@@ -8,21 +10,26 @@ function fillTheTree(str, word, x, y, parent)
 	// finding radius
 	var r = Math.min(h - y, Math.min(w - x, y));
 	
-	for(i=0; i<list.length; i++)
+	if(expandeds.indexOf(word) == -1)
 	{
-		var offset = 5;
-		var divTop = y + r * Math.cos(Math.PI * (i + offset) / (list.length + 2 * offset -1));
-		var divRight = x + r * Math.sin(Math.PI * (i + offset) / (list.length + 2 * offset -1));
-		
-		// creat the word in space
-		var div = document.createElement('div');
-		div.className = "word";
-		div.id = word + i;
-		div.style.top = divTop + "px";
-		div.style.right = divRight + "px";
-		div.innerHTML = '<a onclick="fillWithWord(this.innerHTML, ' + divRight + ', ' + divTop + ', \'' + (word + i) + '\')">' + list[i] + "</a>";
-		document.getElementsByTagName('body')[0].appendChild(div);
+		for(i=0; i<list.length; i++)
+		{
+			var offset = 5;
+			var divTop = y + r * Math.cos(Math.PI * (i + offset) / (list.length + 2 * offset -1));
+			var divRight = x + r * Math.sin(Math.PI * (i + offset) / (list.length + 2 * offset -1));
+			
+			// creat the word in space
+			var div = document.createElement('div');
+			div.className = "word";
+			div.id = word + i;
+			div.style.top = divTop + "px";
+			div.style.right = divRight + "px";
+			div.innerHTML = '<a onclick="fillWithWord(this.innerHTML, ' + divRight + ', ' + divTop + ', \'' + (word + i) + '\')">' + list[i] + "</a>";
+			document.getElementsByTagName('body')[0].appendChild(div);
+		}
 	}
+		
+	expandeds.push(word);
 	
 	for(i=0; i<list.length; i++)
 	{
